6e11cee0964e9f982eefdbbb742afd283c2efd58
deepak <deepsarora262@gmail.com>
2015-02-11 08:02:11 +0530

    delete a install folder

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kartzill.com/admin/');
+define('HTTP_CATALOG', 'http://kartzill.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kartzill.com/admin/');
+define('HTTPS_CATALOG', 'http://kartzill.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/forge/kartzill.com/public/kart-zill/admin/');
+define('DIR_SYSTEM', '/home/forge/kartzill.com/public/kart-zill/system/');
+define('DIR_LANGUAGE', '/home/forge/kartzill.com/public/kart-zill/admin/language/');
+define('DIR_TEMPLATE', '/home/forge/kartzill.com/public/kart-zill/admin/view/template/');
+define('DIR_CONFIG', '/home/forge/kartzill.com/public/kart-zill/system/config/');
+define('DIR_IMAGE', '/home/forge/kartzill.com/public/kart-zill/image/');
+define('DIR_CACHE', '/home/forge/kartzill.com/public/kart-zill/system/cache/');
+define('DIR_DOWNLOAD', '/home/forge/kartzill.com/public/kart-zill/system/download/');
+define('DIR_UPLOAD', '/home/forge/kartzill.com/public/kart-zill/system/upload/');
+define('DIR_LOGS', '/home/forge/kartzill.com/public/kart-zill/system/logs/');
+define('DIR_MODIFICATION', '/home/forge/kartzill.com/public/kart-zill/system/modification/');
+define('DIR_CATALOG', '/home/forge/kartzill.com/public/kart-zill/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '104.131.107.152');
+define('DB_USERNAME', 'forge');
+define('DB_PASSWORD', 'vNgnyVEKVFShH0hzz6uT');
+define('DB_DATABASE', 'kartzill');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kartzill.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kartzill.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/forge/kartzill.com/public/kart-zill/catalog/');
+define('DIR_SYSTEM', '/home/forge/kartzill.com/public/kart-zill/system/');
+define('DIR_LANGUAGE', '/home/forge/kartzill.com/public/kart-zill/catalog/language/');
+define('DIR_TEMPLATE', '/home/forge/kartzill.com/public/kart-zill/catalog/view/theme/');
+define('DIR_CONFIG', '/home/forge/kartzill.com/public/kart-zill/system/config/');
+define('DIR_IMAGE', '/home/forge/kartzill.com/public/kart-zill/image/');
+define('DIR_CACHE', '/home/forge/kartzill.com/public/kart-zill/system/cache/');
+define('DIR_DOWNLOAD', '/home/forge/kartzill.com/public/kart-zill/system/download/');
+define('DIR_UPLOAD', '/home/forge/kartzill.com/public/kart-zill/system/upload/');
+define('DIR_MODIFICATION', '/home/forge/kartzill.com/public/kart-zill/system/modification/');
+define('DIR_LOGS', '/home/forge/kartzill.com/public/kart-zill/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '104.131.107.152');
+define('DB_USERNAME', 'forge');
+define('DB_PASSWORD', 'vNgnyVEKVFShH0hzz6uT');
+define('DB_DATABASE', 'kartzill');
+define('DB_PREFIX', 'oc_');
