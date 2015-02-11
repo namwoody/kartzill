89fa8c3221d4c43bf2e7f3d7433edc5ca15d31f8
namwoody <deepsarora262@gmail.com>
2015-02-10 22:04:32 -0500

    delete install

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8000/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/unobab/Desktop/sites/kartzill/catalog/');
+define('DIR_SYSTEM', '/Users/unobab/Desktop/sites/kartzill/system/');
+define('DIR_LANGUAGE', '/Users/unobab/Desktop/sites/kartzill/catalog/language/');
+define('DIR_TEMPLATE', '/Users/unobab/Desktop/sites/kartzill/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/unobab/Desktop/sites/kartzill/system/config/');
+define('DIR_IMAGE', '/Users/unobab/Desktop/sites/kartzill/image/');
+define('DIR_CACHE', '/Users/unobab/Desktop/sites/kartzill/system/cache/');
+define('DIR_DOWNLOAD', '/Users/unobab/Desktop/sites/kartzill/system/download/');
+define('DIR_UPLOAD', '/Users/unobab/Desktop/sites/kartzill/system/upload/');
+define('DIR_MODIFICATION', '/Users/unobab/Desktop/sites/kartzill/system/modification/');
+define('DIR_LOGS', '/Users/unobab/Desktop/sites/kartzill/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'kart');
+define('DB_PREFIX', 'oc_');
